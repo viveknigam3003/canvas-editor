76c4fea3d4398309ec0cebfcf95ce26a6775f931
RocketierYashank <yashank@rocketium.com>
2023-12-15 15:29:52 +0530

    chore: adding font styles

--- src/modules/text/Panel.tsx
+++ src/modules/text/Panel.tsx
@@ -18,6 +18,34 @@ const TextPanel = ({ canvas, currentSelectedElements, artboardRef }: PanelProps)
 	const [value, setValue] = useState('');
 	const [fontWeight, setFontWeight] = useState('regular');
 	const currentFont = fontList.find(font => font.family === value) as Font;
+	// Add these states at the beginning of your component
+	const [isBold, setIsBold] = useState(false);
+	const [isItalic, setIsItalic] = useState(false);
+	const [isUnderline, setIsUnderline] = useState(false);
+	const [isSuperscript, setIsSuperscript] = useState(false);
+	const [isSubscript, setIsSubscript] = useState(false);
+
+	useEffect(() => {
+		const textElement = currentSelectedElements?.[0] as fabric.Text;
+		if (textElement) {
+			const isBold = textElement.fontWeight === 'bold';
+			setIsBold(isBold);
+
+			const isItalic = textElement.fontStyle === 'italic';
+			setIsItalic(isItalic);
+
+			const isUnderline = textElement.underline;
+			setIsUnderline(isUnderline || false);
+
+			// Check if the text is superscript or subscript by checking the fontSize and deltaY properties
+			const selectedStyles = textElement.getSelectionStyles(0, textElement?.text?.length);
+			const isSuperscript = selectedStyles.some(style => style.fontSize && style.deltaY < 0);
+			setIsSuperscript(isSuperscript);
+
+			const isSubscript = selectedStyles.some(style => style.fontSize && style.deltaY > 0);
+			setIsSubscript(isSubscript);
+		}
+	}, [currentSelectedElements]);
 
 	useEffect(() => {
 		fetch('/fonts.json')
@@ -32,8 +60,10 @@ const TextPanel = ({ canvas, currentSelectedElements, artboardRef }: PanelProps)
 		const textElement = currentSelectedElements?.[0] as fabric.Text;
 		if (textElement.fontWeight === 'bold') {
 			textElement.set({ fontWeight: 'normal' });
+			setIsBold(false);
 		} else {
 			textElement.set({ fontWeight: 'bold' });
+			setIsBold(true);
 		}
 		canvas.requestRenderAll();
 	};
@@ -41,8 +71,10 @@ const TextPanel = ({ canvas, currentSelectedElements, artboardRef }: PanelProps)
 		const textElement = currentSelectedElements?.[0] as fabric.Text;
 		if (textElement.fontStyle === 'italic') {
 			textElement.set({ fontStyle: 'normal' });
+			setIsItalic(false);
 		} else {
 			textElement.set({ fontStyle: 'italic' });
+			setIsItalic(true);
 		}
 		canvas.requestRenderAll();
 	};
@@ -50,20 +82,111 @@ const TextPanel = ({ canvas, currentSelectedElements, artboardRef }: PanelProps)
 		const textElement = currentSelectedElements?.[0] as fabric.Text;
 		if (textElement.underline) {
 			textElement.set({ underline: false });
+			setIsUnderline(false);
 		} else {
 			textElement.set({ underline: true });
+			setIsUnderline(true);
 		}
 		canvas.requestRenderAll();
 	};
 	const toggleSuperscript = () => {
 		const textElement = currentSelectedElements?.[0] as fabric.Textbox;
-		const start = textElement.selectionStart;
-		const end = textElement.selectionEnd;
-		const selectedStyles = textElement.getSelectionStyles(start, end);
-		const selectedText = textElement?.text?.slice(start, end);
+		const start = textElement.selectionStart || 0;
+		const end = textElement.selectionEnd || 1;
 
-		console.log('Selected text:', selectedText);
-		console.log('Selected styles:', selectedStyles);
+		// Get the current styles of the selected text
+		const selectedStyles = textElement.getSelectionStyles(start, end);
+
+		// Check if the selected text is superscript by checking the fontSize and deltaY properties
+		const isSuperscript = selectedStyles.some(style => style.fontSize && style.deltaY < 0);
+
+		// Check if the selected text is subscript
+		const isSubscript = selectedStyles.some(style => style.fontSize && style.deltaY > 0);
+
+		if (isSubscript) {
+			// If subscript is active, remove it
+			for (const line in textElement.styles) {
+				if (textElement.styles[line]) {
+					for (let i = start; i < end; i++) {
+						if (textElement.styles[line][i]) {
+							textElement.styles[line][i] = {};
+						}
+					}
+				}
+			}
+			setIsSubscript(false);
+		}
+
+		if (isSuperscript) {
+			// If the selected text is superscript, remove the superscript
+			for (const line in textElement.styles) {
+				if (textElement.styles[line]) {
+					for (let i = start; i < end; i++) {
+						if (textElement.styles[line][i]) {
+							textElement.styles[line][i] = {};
+						}
+					}
+				}
+			}
+			setIsSuperscript(false);
+		} else {
+			// If the selected text is not superscript, add superscript
+			textElement.setSuperscript(start || 0, end || 1);
+			setIsSuperscript(true);
+		}
+
+		// Force the textbox to re-render
+		textElement.canvas?.requestRenderAll();
+	};
+
+	const toggleSubscript = () => {
+		const textElement = currentSelectedElements?.[0] as fabric.Textbox;
+		const start = textElement.selectionStart || 0;
+		const end = textElement.selectionEnd || 1;
+
+		// Get the current styles of the selected text
+		const selectedStyles = textElement.getSelectionStyles(start, end);
+
+		// Check if the selected text is subscript
+		const isSubscript = selectedStyles.some(style => style.fontSize && style.deltaY > 0);
+
+		// Check if the selected text is superscript
+		const isSuperscript = selectedStyles.some(style => style.fontSize && style.deltaY < 0);
+
+		if (isSuperscript) {
+			// If superscript is active, remove it
+			for (const line in textElement.styles) {
+				if (textElement.styles[line]) {
+					for (let i = start; i < end; i++) {
+						if (textElement.styles[line][i]) {
+							textElement.styles[line][i] = {};
+						}
+					}
+				}
+			}
+			setIsSuperscript(false);
+		}
+
+		if (isSubscript) {
+			// If the selected text is subscript, remove the subscript
+			for (const line in textElement.styles) {
+				if (textElement.styles[line]) {
+					for (let i = start; i < end; i++) {
+						if (textElement.styles[line][i]) {
+							textElement.styles[line][i] = {};
+						}
+					}
+				}
+			}
+			setIsSubscript(false);
+		} else {
+			// If the selected text is not subscript, add subscript
+			textElement.setSubscript(start || 0, end || 1);
+			setIsSubscript(true);
+		}
+
+		// Force the textbox to re-render
+		textElement.canvas?.requestRenderAll();
 	};
 
 	return (
@@ -144,31 +267,70 @@ const TextPanel = ({ canvas, currentSelectedElements, artboardRef }: PanelProps)
 							toggleBold();
 						}}
 					>
-						<IconBold size={20} />
+						<IconBold
+							size={20}
+							style={{
+								borderRadius: 4,
+								backgroundColor: isBold ? '#7950f2' : 'transparent',
+								color: isBold ? 'white' : 'grey',
+							}}
+						/>
 					</ActionIcon>
 					<ActionIcon
 						onClick={() => {
 							toggleItalic();
 						}}
 					>
-						<IconItalic size={20} />
+						<IconItalic
+							size={20}
+							style={{
+								borderRadius: 4,
+								backgroundColor: isItalic ? '#7950f2' : 'transparent',
+								color: isItalic ? 'white' : 'grey',
+							}}
+						/>
 					</ActionIcon>
 					<ActionIcon
 						onClick={() => {
 							toggleUndeline();
 						}}
 					>
-						<IconUnderline size={20} />
+						<IconUnderline
+							size={20}
+							style={{
+								borderRadius: 4,
+								backgroundColor: isUnderline ? '#7950f2' : 'transparent',
+								color: isUnderline ? 'white' : 'grey',
+							}}
+						/>
 					</ActionIcon>
 					<ActionIcon
 						onClick={() => {
 							toggleSuperscript();
 						}}
 					>
-						<IconSuperscript size={20} />
+						<IconSuperscript
+							size={20}
+							style={{
+								borderRadius: 4,
+								backgroundColor: isSuperscript ? '#7950f2' : 'transparent',
+								color: isSuperscript ? 'white' : 'grey',
+							}}
+						/>
 					</ActionIcon>
-					<ActionIcon>
-						<IconSubscript size={20} />
+					<ActionIcon
+						onClick={() => {
+							toggleSubscript();
+						}}
+					>
+						<IconSubscript
+							size={20}
+							style={{
+								borderRadius: 4,
+								backgroundColor: isSubscript ? '#7950f2' : 'transparent',
+								color: isSubscript ? 'white' : 'grey',
+							}}
+						/>
 					</ActionIcon>
 				</div>
 			</Stack>
