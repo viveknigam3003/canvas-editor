859de059395009067a844fa4b1de75eb1f2f151b
Vivek Nigam <vivek.nigam@rocketium.com>
2023-12-21 16:01:57 +0530

    feat: installing ffmpeg

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
+	optimizeDeps: {
+		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+	},
+	server: {
+		headers: {
+			'Cross-Origin-Opener-Policy': 'same-origin',
+			'Cross-Origin-Embedder-Policy': 'require-corp',
+		},
+	},
 });
