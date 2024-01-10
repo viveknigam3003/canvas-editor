b99dfa7e9e35fe5c827516c7959030fe2acdec5a
Vivek Nigam <vivek.nigam@rocketium.com>
2024-01-10 17:58:44 +0530

    feat: using parsers

--- src/modules/keyboard/ShortcutKeys.tsx
+++ src/modules/keyboard/ShortcutKeys.tsx
@@ -1,5 +1,5 @@
 import { Kbd, Text } from '@mantine/core';
-import { getAltKey, getModKey } from '../utils/keyboard';
+import { parseShortcutKey } from './helpers';
 
 interface ShortcutKeysProps {
 	keyString: string;
@@ -9,15 +9,7 @@ const ShortcutKeys = ({ keyString }: ShortcutKeysProps) => {
 	if (!keyString) return null;
 	const keys = keyString.split('+');
 
-	const parsedKeys = keys.map(key => {
-		if (key === 'mod' || key.toLowerCase() === 'meta' || key.toLowerCase() === 'ctrl') {
-			return <Kbd key={key}>{getModKey()}</Kbd>;
-		}
-		if (key.toLowerCase() === 'alt') {
-			return <Kbd key={key}>{getAltKey()}</Kbd>;
-		}
-		return <Kbd key={key}>{key.toUpperCase()}</Kbd>;
-	});
+	const parsedKeys = keys.map(key => <Kbd key={key}>{parseShortcutKey(key)}</Kbd>);
 
 	// Insert a + between each key
 	const parsedKeysWithPluses = [];
