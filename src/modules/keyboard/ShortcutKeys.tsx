c917daa984abf2dd827bd3c977c30184dde5680a
Vivek Nigam <vivek.nigam@rocketium.com>
2024-01-10 16:58:57 +0530

    feat: added shortcut keys component

---
+++ src/modules/keyboard/ShortcutKeys.tsx
@@ -0,0 +1,34 @@
+import { Kbd, Text } from '@mantine/core';
+import { getAltKey, getModKey } from '../utils/keyboard';
+
+interface ShortcutKeysProps {
+	keyString: string;
+}
+
+const ShortcutKeys = ({ keyString }: ShortcutKeysProps) => {
+	if (!keyString) return null;
+	const keys = keyString.split('+');
+
+	const parsedKeys = keys.map(key => {
+		if (key === 'mod' || key.toLowerCase() === 'meta' || key.toLowerCase() === 'ctrl') {
+			return <Kbd>{getModKey()}</Kbd>;
+		}
+		if (key.toLowerCase() === 'alt') {
+			return <Kbd>{getAltKey()}</Kbd>;
+		}
+		return <Kbd>{key.toUpperCase()}</Kbd>;
+	});
+
+	// Insert a + between each key
+	const parsedKeysWithPluses = [];
+	for (let i = 0; i < parsedKeys.length; i++) {
+		parsedKeysWithPluses.push(parsedKeys[i]);
+		if (i !== parsedKeys.length - 1) {
+			parsedKeysWithPluses.push(' + ');
+		}
+	}
+
+	return <Text size={12}>{parsedKeysWithPluses}</Text>;
+};
+
+export default ShortcutKeys;
