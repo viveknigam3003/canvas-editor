45e127416cc60dc7fd7268320b17e718ecc8e015
Vivek Nigam <vivek.nigam@rocketium.com>
2024-01-12 00:26:19 +0530

    feat: trying worker without blob

--- src/modules/animate/index.tsx
+++ src/modules/animate/index.tsx
@@ -58,7 +58,7 @@ const Animation: React.FC<AnimationProps> = ({ currentSelectedElements, saveArtb
 		const res = await ffmpeg.load({
 			coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
 			wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-			workerURL: await toBlobURL(`${baseURL}/ffmpeg-core.worker.js`, 'text/javascript'),
+			workerURL: `${baseURL}/ffmpeg-core.worker.js`,
 		});
 		console.log('ffmpeg loaded', res);
 		setLoaded(true);
